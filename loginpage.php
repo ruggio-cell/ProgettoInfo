43ac8904bedc0ffdb04becdf611a0e619bc3214c
DigioMatthy <mattyas2015@gmail.com>
2024-05-17 11:58:01 +0200

    big update

--- loginpage.php
+++ loginpage.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="resources/style.css">
     <title>Login</title>
 </head>
 <body>
+    <form action="POST">
+        <label for="email">Email:</label><br>
+        <input type="email"><br>
+        <label for="password">Password:</label><br>
+        <input type="passoword">
+    </form>
     
 </body>
 </html>
